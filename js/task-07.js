a881b36805e8fab155652811b91fe5e0945c62ad
Gunnar97 <135347097+Gunnar97@users.noreply.github.com>
2023-08-10 13:34:55 +0300

    1

--- js/task-07.js
+++
@@ -1,7 +0,0 @@
-const fontSizeControl = document.querySelector('#font-size-control');
-const text = document.querySelector('#text');
-fontSizeControl.addEventListener('input', changeFontSize);
-
-function changeFontSize(eve) {
-    text.style.fontSize = eve.target.value + 'px';
-}
